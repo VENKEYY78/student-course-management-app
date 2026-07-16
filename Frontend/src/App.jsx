fb11b6a8dbb2f4788b8d73655547434c29ede2d5
Srinivasan <srinivasanb656@gmail.com>
2026-07-17 00:42:14 +0530

    Add Browse Courses page UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import LandingPage from './pages/LandingPage/LandingPage.jsx';
 import LoginPage from './pages/LoginPage/Login.jsx';
 import MyCourses from './pages/StudentPage/MyCourses.jsx';
+import BrowseCourses from './pages/StudentPage/BrowseCourses.jsx';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ const App = () => (
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/my-courses" element={<MyCourses />} />
+            <Route path="/browse-courses" element={<BrowseCourses />} />
         </Routes>
     </BrowserRouter>
 );
